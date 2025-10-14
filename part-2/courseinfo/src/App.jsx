71d6df1ecdfc8d25ce5628e1dfa64b3ee432c8cf
tahakinci <tahakinci29@gmail.com>
2025-10-14 07:18:18 +0300

    Exercises 2.1 - 2.5 complated

--- part-2/courseinfo/src/App.jsx
+++ part-2/courseinfo/src/App.jsx
@@ -1,42 +1,46 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
-import { Fragment } from "react";
-
-const Course = (props) => {
-  let total;
-  return (
-    <>
-      {props.courses.map((course) => {
-        total = course.parts.reduce((acc, curr) => acc + curr.exercises, 0);
-        return (
-          <Fragment key={course.name}>
-            <Header header={course.name} />
-            <Content course={course} />
-          </Fragment>
-        );
-      })}
-      <Total total={total} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
-  const course = [
+  const courses = [
     {
       name: "Half Stack application development",
+      id: 1,
       parts: [
         {
           name: "Fundamentals of React",
           exercises: 10,
+          id: 1,
         },
         {
           name: "Using props to pass data",
           exercises: 7,
+          id: 2,
         },
         {
           name: "State of a component",
           exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
         },
       ],
     },
@@ -44,7 +48,7 @@ const App = () => {
 
   return (
     <div>
-      <Course courses={course} />
+      <Course courses={courses} />
     </div>
   );
 };
