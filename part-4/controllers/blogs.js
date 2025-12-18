96046eff9d12e92320dc552751587a3e844328ea
SYSTEMSOFT\muhammed.akinci <muhammed.akinci@sistemyazilim.com>
2025-12-18 18:10:25 +0300

    Exercises 4.15 - 4.19 complated

--- part-4/controllers/blogs.js
+++ part-4/controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require("express").Router()
+const jwt = require("jsonwebtoken")
 const Blog = require('../models/blog')
+const User = require("../models/user")
 
 blogsRouter.get('/', async (req, res) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
     res.json(blogs)
 })
 
@@ -16,17 +18,36 @@ blogsRouter.get("/:id", async (req, res) => {
     }
 })
 
+const getTokenFrom = req => {
+    const auth = req.get("authorization")
+    if (auth && auth.startsWith("Bearer ", "")) {
+        return auth.replace("Bearer ", "")
+    }
+    return null
+}
+
 blogsRouter.post("/", async (req, res, next) => {
     const body = req.body
+    const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+
+    const user = await User.findById(decodedToken.id)
+
+    if (!user) {
+        return res.status(400).json({ error: 'userId missing or not valid' })
+    }
 
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body?.likes ?? 0
+        likes: body?.likes ?? 0,
+        user: user._id
     })
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     res.status(201).json(savedBlog)
 })
 
--- part-4/utils/middleware.js
+++ part-4/utils/middleware.js
@@ -1,24 +1,30 @@
 const logger = require('./logger')
 
-const requestLogger = (request, response, next) => {
-    logger.info('Method:', request.method)
-    logger.info('Path:  ', request.path)
-    logger.info('Body:  ', request.body)
+const requestLogger = (req, res, next) => {
+    logger.info('Method:', req.method)
+    logger.info('Path:  ', req.path)
+    logger.info('Body:  ', req.body)
     logger.info('---')
     next()
 }
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
 }
 
-const errorHandler = (error, request, response, next) => {
+const errorHandler = (error, req, res, next) => {
     logger.error(error.message)
 
     if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
+        return res.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
+        return res.status(400).json({ error: error.message })
+    } else if (error.name === "MongoServerError" && error.message.includes("E11000 duplicate key error")) {
+        return res.status(400).json({ error: 'expected `username` to be unique' })
+    } else if (error.name === "JsonWebTokenError") {
+        return res.status(401).json({ error: "token invalid" })
+    } else if (error.name === "TokenExpiredError") {
+        return res.status(401).json({ error: "token expired" })
     }
 
     next(error)
