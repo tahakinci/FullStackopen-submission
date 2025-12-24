a74eedc9719162ec68f69684d268e5af660aeb0b
tahakinci <tahakinci29@gmail.com>
2025-12-24 22:10:20 +0300

    setup

--- part-2/courseinfo/src/components/Header.jsx
+++
@@ -1,3 +0,0 @@
-const Header = (props) => <h1>{props.header}</h1>;
-
-export default Header;
--- part-2/phonebook/src/components/Numbers.jsx
+++
@@ -1,9 +0,0 @@
-const Numbers = ({ person, handleDelete }) => {
-  return (
-    <>
-      {person.name} - {person.number} <button onClick={handleDelete}>delete</button>
-    </>
-  );
-};
-
-export default Numbers;
